f5cfd9658f91c1a3c12652a81951d29e628eb148
SharadChaturvedi <101339357+SharadChaturvedi@users.noreply.github.com>
2024-02-25 22:51:34 +0530

    Add files via upload

---
+++ pgconstructors_usingSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_day2
+{
+    
+    internal class pgconstructors_usingSwitch
+    {
+        public void checkinggrades_switch()
+        {
+            char grade; int math;
+            Console.WriteLine("Enter Grade and maths Score");
+            grade = Convert.ToChar(Console.ReadLine());
+            math = Convert.ToInt32(Console.ReadLine());
+            switch (grade)
+            {
+                case 'O':
+                case 'o':
+                    Console.WriteLine("outstanding");
+                    break;
+                case 'A':
+                case 'a':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                case 'b':
+                    Console.WriteLine("Very Good");
+                    break;
+                case 'C':
+                case 'c'
+:
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                case 'd':
+                    Console.WriteLine("Can Improve");
+                    break;
+                default:
+                    Console.WriteLine("invalid Grade");
+                    break;
+                }
+            }
+        }
+    }
---
+++ program_constructors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_day2
+{
+    internal class Program_constructors
+    {
+
+        static void Main(string[] args)
+        {
+            pgconstructors_usingSwitch pg = new pgconstructors_usingSwitch();
+            pg.checkinggrades_switch();
+            CheckingGrades();
+        }
+
+
+
+        //if_else 
+        public static void CheckingGrades()
+        {
+            char grades;
+            int maths = 500;
+            Console.WriteLine("Enter the Grades O/A/B/C");
+            grades = Convert.ToChar(Console.ReadLine());
+
+            if
+                ((grades == 'O' || grades == 'o') && (maths > 90))
+                Console.WriteLine("outstanding");
+            else if
+                ((grades == 'A'))
+                Console.WriteLine("Excelent");
+            else if
+                (grades == 'B')
+                Console.WriteLine("Very Good ");
+            else if
+                (grades == 'C')
+                Console.WriteLine("Good");
+
+            else
+                Console.WriteLine("Invalid Grade");
+
+        }
+
+
+
+
+
+    }
+}
+
